1958c4d5b4affb293e545a15046f1ade9ff97edc
Enzo <enzoparanedaniela00@gmail.com>
2025-04-11 20:09:21 +0800

    first commit

---
+++ javascript/register.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBd4Dxe_gZ4kL1cd2_fCSn0u2BXSzNLHbw",
+  authDomain: "chap-application-7fbe1.firebaseapp.com",
+  projectId: "chap-application-7fbe1",
+  storageBucket: "chap-application-7fbe1.firebasestorage.app",
+  messagingSenderId: "805455096714",
+  appId: "1:805455096714:web:80508ce675395f273cb673"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
